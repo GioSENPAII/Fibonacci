3d6f598fa3c4de07438cdb24eff457fa1ddef515
GioSENPAII <89473339+GioSENPAII@users.noreply.github.com>
2023-04-24 19:10:09 -0600

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -5,9 +5,8 @@ def fibonacci_recursivo(n):
         return 1
     else:
         return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
-    
+ fibonacci_recursivo(5)
 #De acuerdo al teorema del Maestro este algoritmo tiene una complejidad de O(logn) ya que primero "a" tiene una cantidad de llamados recursivos de 2, después n/b es igual a n
-#por lo tanto b=1 y finalmente tenemos que O(1) ya que las operaciones son en tiempo constante, por lo tanto c=0
+#por lo tanto b=5/3 y finalmente tenemos que O(1) ya que las operaciones son en tiempo constante, por lo tanto c=0
 #Finalmente sustituyendo tenemos que O(n^log1(2)).
 
-#Sin embargo trate de comprobar mi resultado y algunos sitios indicaban que la complejidad es de O(logn) y ya de ahi no pude ver el porque de este resultado.
